a1d52e54dd2e4e8d3310ca648ebf33ef56d22107
vito <support@whiteoffice.site>
2021-03-11 12:42:27 -0800

    Initial Commit

---
+++ Todoist/TodoistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoistApp.swift
+//  Todoist
+//
+//  Created by Eusebio Aguayo on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
